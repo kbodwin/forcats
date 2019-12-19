e4762f4836c2a4eed6fc706058ade30a28fabbe5
Kelly N. Bodwin <kbodwin@calpoly.edu>
2019-12-19 15:52:30 -0800

    fix issue with multiple factors

--- R/cross.R
+++ R/cross.R
@@ -24,7 +24,8 @@ fct_cross <- function(.f, ..., sep = ":", keep_empty = FALSE, preserve_orders = 
     return(.f)
   }
 
-  .data <- lapply(tibble::tibble(.f, !!!flist), check_factor)
+  .data <- suppressWarnings(tibble::tibble(.f, !!!flist, .name_repair = "universal"))
+  .data <- lapply(.data, check_factor)
   newf <- rlang::exec(paste, .f, !!!flist, sep = sep)
 
   if (keep_empty) {
